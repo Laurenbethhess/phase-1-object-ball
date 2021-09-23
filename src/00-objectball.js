70baf1657cce33cd750835ead06451b88fb41ef0
Lauren Hess <laurenbethhess@gmail.com>
2021-09-23 12:06:14 -0400

    where we left off

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -3,16 +3,16 @@ function gameObject(){
         home: {
             teamName: 'Brooklyn Nets',
             colors: ['Black', 'White'],
-            players: {
+            player: {
                 'Alan Anderson': {
-                    number: 4,
-                    shoe: 18,
-                    points: 10,
-                    rebounds: 1,
-                    assists: 1,
-                    steals: 2,
-                    blocks:7,
-                    slamDunks: 2
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks:1,
+                    slamDunks: 1
                     },
                 'Reggie Evans': {
                     number: 0,
@@ -25,6 +25,62 @@ function gameObject(){
                     slamDunks: 10
                     },
                 'Brook Lopez': {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7
+                    },
+                'Mason Plumlee': {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15
+                    },
+                'Jason Terry': {
+                    number: 31,
+                    shoe: 15, 
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamDunks: 1,
+                    }
+                }
+        },
+        away: {
+            teamName:  'Charlotte Hornets',
+            colors:  ['Turquoise', 'Purple'],
+            player: {
+                'Jeff Adrien': {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks:7,
+                    slamDunks: 2
+                    },
+                'Bismak Biyombo':{
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10
+                    },
+                'DeSagna Diop':{
                     number: 2,
                     shoe: 14,
                     points: 24,
@@ -34,17 +90,17 @@ function gameObject(){
                     blocks: 5,
                     slamDunks: 5
                     },
-                'Mason Plumlee': {
-                    number: 8,
-                    shoe: 15,
-                    points: 33,
-                    rebounds: 3,
-                    assists: 2,
-                    steals: 1,
-                    blocks: 1,
-                    slamDunks: 0
+                'Ben Gordon':{
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5
                     },
-                'Jason Terry': {
+                'Brendan Haywood':{
                     number: 33,
                     shoe: 15, 
                     points: 6,
@@ -55,65 +111,28 @@ function gameObject(){
                     slamDunks: 12
                     }
                 }
-        },
-        away: {
-            teamName:  'Charlotte Hornets',
-            colors:  'Turquoise, Purple',
-            players: {
-                'Alan Anderson': {
-                    number: 1,
-                    shoe: 2,
-                    points: 3,
-                    rebounds: 4,
-                    assists: 5,
-                    steals: 6,
-                    blocks: 7,
-                    slamDunks: 8
-                    },
-                'Reggie Evans':{
-                    number: 1,
-                    shoe: 2,
-                    points: 3,
-                    rebounds: 4,
-                    assists: 5,
-                    steals: 6,
-                    blocks: 7,
-                    slamDunks: 8
-                    },
-                'Brook Lopez':{
-                    number: 1,
-                    shoe: 2,
-                    points: 3,
-                    rebounds: 4,
-                    assists: 5,
-                    steals: 6,
-                    blocks: 7,
-                    slamDunks: 8
-                    },
-                'Mason Plumlee':{
-                    number: 1,
-                    shoe: 2,
-                    points: 3,
-                    rebounds: 4,
-                    assists: 5,
-                    steals: 6,
-                    blocks: 7,
-                    slamDunks: 8
-                    },
-                'Jason Terry':{
-                    number: 1,
-                    shoe: 2,
-                    points: 3,
-                    rebounds: 4,
-                    assists: 5,
-                    steals: 6,
-                    blocks: 7,
-                    slamDunks: 8
-                    }
-                }
             }
         }
         return object
     }
    
 console.log(gameObject())
+
+function numPointsScored(name) {
+    let game = gameObject();
+    for (let gameKey in game) {
+  
+        let teamObj = game[gameKey];
+        console.log(teamObj)
+        for (let teamKey in teamObj) {
+       
+            let data = teamKey.player;
+            console.log(data)
+            for (let key in data) {
+                if(name===key)
+                return data[key].points
+            }
+        }
+    } 
+}
+console.log(numPointsScored('Brendan Haywood'))
