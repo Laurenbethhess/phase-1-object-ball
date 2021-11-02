8868f7f00bb63ea42a79f5a34d1a3e1beb9b1bee
Lauren Hess <laurenbethhess@gmail.com>
2021-11-02 13:16:55 -0400

    commit tree 22ce89a60f73f07f8b510f8334fda1f87a1e04c2

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -3,7 +3,7 @@ function gameObject(){
         home: {
             teamName: 'Brooklyn Nets',
             colors: ['Black', 'White'],
-            player: {
+            players: {
                 'Alan Anderson': {
                     number: 0,
                     shoe: 16,
@@ -15,16 +15,6 @@ function gameObject(){
                     slamDunks: 1
                     },
                 'Reggie Evans': {
-                    number: 0,
-                    shoe: 16,
-                    points: 12,
-                    rebounds: 4,
-                    assists: 7,
-                    steals: 7,
-                    blocks: 15,
-                    slamDunks: 10
-                    },
-                'Brook Lopez': {
                     number: 30,
                     shoe: 14,
                     points: 12,
@@ -34,7 +24,7 @@ function gameObject(){
                     blocks: 12,
                     slamDunks: 7
                     },
-                'Mason Plumlee': {
+                'Brook Lopez': {
                     number: 11,
                     shoe: 17,
                     points: 17,
@@ -43,6 +33,16 @@ function gameObject(){
                     steals: 3,
                     blocks: 1,
                     slamDunks: 15
+                    },
+                'Mason Plumlee': {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5
                     },
                 'Jason Terry': {
                     number: 31,
@@ -59,7 +59,7 @@ function gameObject(){
         away: {
             teamName:  'Charlotte Hornets',
             colors:  ['Turquoise', 'Purple'],
-            player: {
+            players: {
                 'Jeff Adrien': {
                     number: 4,
                     shoe: 18,
@@ -91,14 +91,14 @@ function gameObject(){
                     slamDunks: 5
                     },
                 'Ben Gordon':{
-                    number: 2,
-                    shoe: 14,
-                    points: 24,
-                    rebounds: 12,
-                    assists: 12,
-                    steals: 4,
-                    blocks: 5,
-                    slamDunks: 5
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0
                     },
                 'Brendan Haywood':{
                     number: 33,
@@ -113,24 +113,39 @@ function gameObject(){
                 }
             }
         }
-        return object
-    }
+    return object
+}
    
 console.log(gameObject())
+
+function homeTeamName() {
+    let object = gameObject()
+    return object['home']['teamName']
+}
 
 function numPointsScored(theName) {
     const theGame = gameObject()
     for (const team in theGame){
-        const player = theGame[team].player
+        const player = theGame[team].players
+
+        for (const thePlayer in player) {
+            if (thePlayer === theName) {
+                return player[thePlayer].shoe
+            }
+        }
+    }
+}
+
+function shoeSize(theName) {
+    const theGame = gameObject()
+    for (const team in theGame){
+        const player = theGame[team].players
 
         for (const thePlayer in player) {
             if (thePlayer === theName) {
                 return player[thePlayer].points
             }
-
         }
-
     }
 }
 
-numPointsScored('Brendan Haywood')
